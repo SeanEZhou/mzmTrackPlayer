f59cccfc04d5cbb358d1afcfdac322f67b4b3e07
SeanEZhou <sezhou@andrew.cmu.edu>
2020-12-24 16:56:47 -0800

    migrated files to expo project

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import LandingScreen from "./react/screens/LandingScreen";
+import PlaylistScreen from "./react/screens/PlaylistScreen";
+
+const AppNavigator = createStackNavigator(
+  {
+    Landing: {
+      screen: LandingScreen
+    },
+    Playlist: {
+      screen: PlaylistScreen
+    }
+  },
+  { initialRouteName: "Landing" }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppContainer />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
